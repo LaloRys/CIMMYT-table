342e24d7ade5d988d05422aacfe56a083a674395
DevRys <eduardoreyes55@aragon.unam.mx>
2023-09-19 18:09:04 -0600

    Costum table

---
+++ src/components/Icons.jsx
@@ -0,0 +1,11 @@
+export { BiLinkAlt as Link, BiUnlink as Unlink } from "react-icons/bi";
+export {
+  TbWorldLongitude as Longitude,
+  TbWorldLatitude as Latitude,
+  TbArrowBigLeftFilled as Aleft,
+  TbArrowBigLeftLinesFilled as AleftLines,
+  TbArrowBigRightFilled as Aright,
+  TbArrowBigRightLinesFilled as ArightLines,
+  TbSquareArrowDownFilled as ArrowDown,
+  TbSquareArrowUpFilled as ArrowUp,
+} from "react-icons/tb";
--- src/pages/DataTable.jsx
+++ src/pages/DataTable.jsx
@@ -10,6 +10,18 @@ import {
 import fakeData from "../data/Data.json";
 import classNames from "classnames";
 import { rankItem } from "@tanstack/match-sorter-utils";
+import {
+  Link,
+  Unlink,
+  Latitude,
+  Longitude,
+  Aleft,
+  AleftLines,
+  Aright,
+  ArightLines,
+  ArrowUp,
+  ArrowDown,
+} from "../components/Icons";
 
 const fuzzyFilter = (row, columnId, value, addMeta) => {
   const itemRank = rankItem(row.getValue(columnId), value);
@@ -40,6 +52,18 @@ const DebounceInput = ({ value: keyWord, onChange, ...props }) => {
   );
 };
 
+const getCommentColor = (comment) => {
+  if (comment.startsWith("WRONG")) {
+    return "bg-red-500 text-white px-2 py-1 rounded-lg"; // Rojo si comienza con 'WRONG'
+  } else if (comment.startsWith("OK")) {
+    return "bg-green-500 text-white px-2 py-1 rounded-lg"; // Verde si comienza con 'CORRECT'
+  } else if (comment.startsWith("NOT")) {
+    return "bg-yellow-400 text-white px-2 py-1 rounded-lg"; // Verde si comienza con 'CORRECT'
+  } else {
+    return "text-black"; // Verde en cualquier otro caso
+  }
+};
+
 function DataTable() {
   // console.log(fakeData);
   const [data, setData] = useState(fakeData);
@@ -52,22 +76,39 @@ function DataTable() {
       accessorKey: "ACCENUMB",
       header: () => <span className="hover:text-slate-300">ACCENUMB</span>,
       cell: (info) => <span className="font-semibold">{info.getValue()}</span>,
-    },
-    {
-      accessorKey: "DECLATITUDE",
-      header: () => <span className="hover:text-slate-300">DECLATITUDE</span>,
       cell: (info) => <span className="font-bold">{info.getValue()}</span>,
     },
     {
+      accessorKey: "DECLATITUDE",
+      header: () => (
+        <div className="flex justify-center items-center">
+          <span>DECLATITUDE</span>{" "}
+          <i className="ml-2 text-xl">
+            <Latitude />
+          </i>
+        </div>
+      ),
+      cell: (info) => (
+        <span className="text-blue-400 font-semibold">{info.getValue()}</span>
+      ),
+    },
+    {
       accessorKey: "DECLONGITUDE",
-      header: () => <span>DECLONGITUDE</span>,
+      header: () => (
+        <div className="flex justify-center items-center">
+          <span>DECLONGITUDE</span>{" "}
+          <i className="ml-2 text-xl">
+            <Longitude />
+          </i>
+        </div>
+      ),
+      cell: (info) => (
+        <span className="text-blue-400 font-semibold ">{info.getValue()}</span>
+      ),
     },
     {
       accessorKey: "ORIGCTY",
       header: () => <span>ORIGCTY</span>,
-      cell: (info) => (
-        <span className="text-cyan-400 font-semibold">{info.getValue()}</span>
-      ),
     },
     {
       accessorKey: "Distance",
@@ -80,6 +121,11 @@ function DataTable() {
     {
       accessorKey: "COMMETS",
       header: () => <span>COMMETS</span>,
+      cell: (info) => {
+        const comment = info.getValue() || ""; // Obtén el contenido de la celda
+        const colorClass = getCommentColor(comment); // Determina el color basado en el contenido
+        return <span className={`font-semibold ${colorClass}`}>{comment}</span>;
+      },
     },
     {
       accessorKey: "Suggested coordinates",
@@ -88,9 +134,20 @@ function DataTable() {
     {
       accessorKey: "GRIN",
       header: () => <span>GRIN</span>,
-      cell: (info) => (
-        <a href={info.getValue()} className="text-cyan-400 font-semibold" target="_blank" >{info.getValue()}</a>
-      ),
+      cell: (info) =>
+        info.getValue() ? (
+          <a
+            href={info.getValue()}
+            className="text-blue-500 font-semibold"
+            target="_blank"
+          >
+            <Link className="text-2xl" />
+          </a>
+        ) : (
+          <i className="text-2xl">
+            <Unlink />
+          </i>
+        ),
     },
   ];
 
@@ -115,7 +172,7 @@ function DataTable() {
     },
     initialState: {
       pagination: {
-        pageSize: 15, //Valor para inicializar la pagina
+        pageSize: 10, //Valor para inicializar la pagina
       },
     },
     getCoreRowModel: getCoreRowModel(),
@@ -128,11 +185,10 @@ function DataTable() {
 
   return (
     <div className="px-6 py-4">
-      
       {/* Buscador  */}
       <div className="my-2 flex justify-between">
         {/* Texto de ubicacion */}
-        <div className="text-gray-400 text-sm font-semibold">
+        <div className="text-slate-900 text-sm font-semibold">
           Mostrando de {getStateTable().firstIndex} al{" "}
           {getStateTable().lastIndex} del total de {getStateTable().totalRows}{" "}
           registros
@@ -142,16 +198,16 @@ function DataTable() {
           type="text"
           value={globalFilter ?? ""}
           onChange={(value) => setGlobalFilter(String(value))}
-          className="text-black border-indigo-800 px-2 py-1 rounded-md outline-indigo-600"
+          className="text-black border-2 border-sky-600 px-2 py-1 rounded-md outline-sky-700"
           placeholder="Buscar..."
         />
       </div>
       {/* Buscador  */}
-      <table className="table-auto w-full">
+      <table className="table-auto w-full ">
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr
-              className="border-b border-gray-300 bg-indigo-600"
+              className="border border-slate-500 bg-sky-600 text-white rounded-lg"
               key={headerGroup.id}
             >
               {headerGroup.headers.map((header) => (
@@ -171,8 +227,8 @@ function DataTable() {
                       )}
                       {/* Manejo de estado con iconos */}
                       {{
-                        asc: " 🔼",
-                        desc: " 🔽",
+                        asc: <ArrowUp className="text-xl" />,
+                        desc: <ArrowDown className="text-xl" />,
                       }[header.column.getIsSorted()] ?? null}
                       {/* Manejo de estado con iconos */}
                     </div>
@@ -185,9 +241,12 @@ function DataTable() {
         </thead>
         <tbody>
           {table.getRowModel().rows.map((row) => (
-            <tr className="text-gray-200 hover:bg-gray-500" key={row.id}>
+            <tr
+              className="text-slate-900 hover:bg-slate-200 border border-slate-500"
+              key={row.id}
+            >
               {row.getVisibleCells().map((cell) => (
-                <td className="py-2 px-4" key={cell.id}>
+                <td className="py-2 px-4 border border-slate-500" key={cell.id}>
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               ))}
@@ -202,24 +261,28 @@ function DataTable() {
           <button
             onClick={() => table.setPageIndex(0)}
             disabled={!table.getCanPreviousPage()}
-            className="bg-indigo-600 px-2 py-1 rounded-md hover:bg-indigo-500  disabled:hover:bg-slate-700"
+            className="bg-sky-600 px-1 py-1 rounded-md hover:bg-sky-500 disabled:hover:bg-slate-700"
           >
-            {"<<"}
+            <i className="text-3xl text-white">
+              <AleftLines />
+            </i>
           </button>
           <button
             onClick={() => table.previousPage()}
             disabled={!table.getCanPreviousPage()}
-            className="bg-indigo-600 px-2 py-1 rounded-md hover:bg-indigo-500  disabled:hover:bg-slate-700"
+            className="bg-sky-600 px-1 py-1 rounded-md hover:bg-sky-500  disabled:hover:bg-slate-700"
           >
-            {"<"}
+            <i className="text-2xl text-white">
+              <Aleft />
+            </i>
           </button>
           {/* Numero de pagina */}
           {table.getPageOptions().map((pageNumber, index) => (
             <button
               onClick={() => table.setPageIndex(pageNumber)}
               className={classNames({
-                "bg-indigo-600 px-2.5 py-1 rounded-md hover:bg-indigo-500 font-semibold disabled:hover:bg-slate-700": true,
-                "bg-indigo-100 text-black":
+                "bg-sky-600 text-white px-2.5 py-1 rounded-md hover:bg-sky-500 hover: font-semibold disabled:hover:bg-slate-700": true,
+                "bg-sky-500 text-black px-3.5 py-2 hover:bg-sky-600":
                   pageNumber == table.getState().pagination.pageIndex,
               })}
               key={index}
@@ -231,20 +294,24 @@ function DataTable() {
           <button
             onClick={() => table.nextPage()}
             disabled={!table.getCanNextPage()}
-            className="bg-indigo-600 px-2 py-1 rounded-md hover:bg-indigo-500 disabled:hover:bg-slate-700"
+            className="bg-sky-600 px-1 py-1 rounded-md hover:bg-sky-500 disabled:hover:bg-slate-700"
           >
-            {">"}
+            <i className="text-2xl text-white">
+              <Aright />
+            </i>
           </button>
           <button
             onClick={() => table.setPageIndex(table.getPageCount() - 1)} // Calcular ultima pagina
             disabled={!table.getCanPreviousPage()}
-            className="bg-indigo-600 px-2 py-1 rounded-md hover:bg-indigo-500  disabled:hover:bg-slate-700"
+            className="bg-sky-600 px-1 py-1 rounded-md hover:bg-sky-500 disabled:hover:bg-slate-700"
           >
-            {">>"}
+            <i className="text-3xl text-white">
+              <ArightLines />
+            </i>
           </button>
         </div>
         {/* Arrows de navegacion */}
-        
+
         {/* Numero de paginas */}
         <select
           className="text-black border  border-gray-200 rounded-lg outline-indigo-600"
